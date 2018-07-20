28bd87c5d88801bed71c158423b9e625179949ed
shangkun <wushangkun@chelun.com>
2018-07-20 10:43:22 +0800

    add tt

--- MemoryLeaks/AppDelegate.h
+++ MemoryLeaks/AppDelegate.h
@@ -11,7 +11,19 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+/*
+一、循环引用的产生
 
+内存中和变量有关的分区： 堆、栈、静态区
 
+栈和静态区是操作系统自己管理的
+ 
+我们只需要关注 堆 的内存分配 , 循环引用会导致 堆 里的内存无法正常回收
+
+回收机制：
+
+•    对堆里面的一个对象发送release消息来使其引用计数减一；
+•    查询引用计数表，将引用计数为0的对象dealloc；
+ */
 @end
 
